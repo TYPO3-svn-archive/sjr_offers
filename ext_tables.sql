d87a5ecf27b32975c9eb0cce7d6d5d46ff7fff8f
jocrau <jocrau@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-15 10:52:07 +0000

    [+BUGFIX] SjrOffers: Fixed table definitions.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sjr_offers/trunk@35690 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,55 +1,55 @@
 CREATE TABLE tx_sjroffers_domain_model_organization (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
 	status int(11) unsigned DEFAULT '0' NOT NULL,
-	name varchar(255) NOT NULL,
+	name varchar(255) DEFAULT '' NOT NULL,
 	address text NOT NULL,
-	telephone_number varchar(80) NOT NULL,
-	telefax_number varchar(80) NOT NULL,
-	url varchar(80) NOT NULL,
-	email_address varchar(80) NOT NULL,
+	telephone_number varchar(80) DEFAULT '' NOT NULL,
+	telefax_number varchar(80) DEFAULT '' NOT NULL,
+	url varchar(80) DEFAULT '' NOT NULL,
+	email_address varchar(80) DEFAULT '' NOT NULL,
 	description text NOT NULL,
-	image varchar(255) NOT NULL,
-	contacts int(11) NOT NULL,
-	offers int(11) NOT NULL,
-	administrator int(11),
+	image varchar(255) DEFAULT '' NOT NULL,
+	contacts int(11) DEFAULT '0' NOT NULL,
+	offers int(11) DEFAULT '0' NOT NULL,
+	administrator int(11) DEFAULT '0',
 	
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
 	l18n_parent int(11) DEFAULT '0' NOT NULL,
 	l18n_diffsource mediumblob NOT NULL,
-	access_group int(11) NOT NULL,
+	access_group int(11) DEFAULT '0' NOT NULL,
 
 	PRIMARY KEY (uid),
 	KEY parent (pid),
 );
 
 CREATE TABLE tx_sjroffers_domain_model_offer (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
-	organization int(11) NOT NULL,
-	title varchar(255) NOT NULL,
+	organization int(11) DEFAULT '0' NOT NULL,
+	title varchar(255) DEFAULT '' NOT NULL,
 	teaser text NOT NULL,
 	description text NOT NULL,
 	services text NOT NULL,
 	dates text NOT NULL,
 	venue text NOT NULL,
 	image tinyblob NOT NULL,
-	age_range int(11) NOT NULL,
-	date_range int(11) NOT NULL,
-	attendance_range int(11) NOT NULL,
-	attendance_fees int(11) NOT NULL,
-	contact int(11) NOT NULL,
-	categories int(11) NOT NULL,
-	regions int(11) NOT NULL,
+	age_range int(11) DEFAULT '0' NOT NULL,
+	date_range int(11) DEFAULT '0' NOT NULL,
+	attendance_range int(11) DEFAULT '0' NOT NULL,
+	attendance_fees int(11) DEFAULT '0' NOT NULL,
+	contact int(11) DEFAULT '0' NOT NULL,
+	categories int(11) DEFAULT '0' NOT NULL,
+	regions int(11) DEFAULT '0' NOT NULL,
 	
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE tx_sjroffers_domain_model_offer (
 );
 
 CREATE TABLE tx_sjroffers_domain_model_person (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
 	name varchar(255) DEFAULT '' NOT NULL,
@@ -72,8 +72,8 @@ CREATE TABLE tx_sjroffers_domain_model_person (
 	url varchar(80) DEFAULT '' NOT NULL,
 	email_address varchar(80) DEFAULT '' NOT NULL,
 	
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 
@@ -82,40 +82,40 @@ CREATE TABLE tx_sjroffers_domain_model_person (
 );
 
 CREATE TABLE tx_sjroffers_domain_model_agerange (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
-	minimum_value int(10) unsigned,
-	maximum_value int(10) unsigned,
+	minimum_value int(11) unsigned,
+	maximum_value int(11) unsigned,
 	
 	PRIMARY KEY (uid),
 	KEY parent (pid),
 );
 
 CREATE TABLE tx_sjroffers_domain_model_daterange (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
-	minimum_value int(10) unsigned,
-	maximum_value int(10) unsigned,
+	minimum_value int(11) unsigned,
+	maximum_value int(11) unsigned,
 	
 	PRIMARY KEY (uid),
 	KEY parent (pid),
 );
 
 CREATE TABLE tx_sjroffers_domain_model_attendancerange (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
-	minimum_value int(10) unsigned,
-	maximum_value int(10) unsigned,
+	minimum_value int(11) unsigned,
+	maximum_value int(11) unsigned,
 	
 	PRIMARY KEY (uid),
 	KEY parent (pid),
 );
 
 CREATE TABLE tx_sjroffers_domain_model_attendancefee (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
 	offer int(11) DEFAULT '0' NOT NULL,
@@ -127,15 +127,15 @@ CREATE TABLE tx_sjroffers_domain_model_attendancefee (
 );
 
 CREATE TABLE tx_sjroffers_domain_model_category (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
 	title varchar(255) DEFAULT '' NOT NULL,
 	description text NOT NULL,
-	is_internal tinyint(10) unsigned DEFAULT '0' NOT NULL,
+	is_internal tinyint(11) unsigned DEFAULT '0' NOT NULL,
 		
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -147,15 +147,15 @@ CREATE TABLE tx_sjroffers_domain_model_category (
 );
 
 CREATE TABLE tx_sjroffers_domain_model_status (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
 	title varchar(255) DEFAULT '' NOT NULL,
 	description text NOT NULL,
-	allowed_categories int(11) NOT NULL,
+	allowed_categories int(11) DEFAULT '0' NOT NULL,
 		
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -167,13 +167,13 @@ CREATE TABLE tx_sjroffers_domain_model_status (
 );
 
 CREATE TABLE tx_sjroffers_domain_model_region (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
 	name varchar(255) DEFAULT '' NOT NULL,
 		
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -185,16 +185,16 @@ CREATE TABLE tx_sjroffers_domain_model_region (
 );
 
 CREATE TABLE tx_sjroffers_organization_person_mm (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
-	uid_local int(10) unsigned NOT NULL,
-	uid_foreign int(10) unsigned NOT NULL,
-	sorting int(10) unsigned NOT NULL,
-	sorting_foreign int(10) unsigned NOT NULL,
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 
@@ -203,16 +203,16 @@ CREATE TABLE tx_sjroffers_organization_person_mm (
 );
 
 CREATE TABLE tx_sjroffers_offer_category_mm (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
-	uid_local int(10) unsigned NOT NULL,
-	uid_foreign int(10) unsigned NOT NULL,
-	sorting int(10) unsigned NOT NULL,
-	sorting_foreign int(10) unsigned NOT NULL,
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -224,16 +224,16 @@ CREATE TABLE tx_sjroffers_offer_category_mm (
 );
 
 CREATE TABLE tx_sjroffers_status_category_mm (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
-	uid_local int(10) unsigned NOT NULL,
-	uid_foreign int(10) unsigned NOT NULL,
-	sorting int(10) unsigned NOT NULL,
-	sorting_foreign int(10) unsigned NOT NULL,
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
@@ -245,16 +245,16 @@ CREATE TABLE tx_sjroffers_status_category_mm (
 );
 
 CREATE TABLE tx_sjroffers_offer_region_mm (
-	uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	
-	uid_local int(10) unsigned NOT NULL,
-	uid_foreign int(10) unsigned NOT NULL,
-	sorting int(10) unsigned NOT NULL,
-	sorting_foreign int(10) unsigned NOT NULL,
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 	
-	tstamp int(10) unsigned NOT NULL,
-	crdate int(10) unsigned NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	hidden tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
